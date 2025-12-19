4fcdf2d56a26548c769581b499e86872874d84c8
Bikkramm <144205863+Bikkramm@users.noreply.github.com>
2025-12-19 18:01:18 +0530

    Create to-load-CSS.php

---
+++ to-load-CSS.php
@@ -0,0 +1,16 @@
+my-theme/
+├── assets/
+│   └── css/
+│       └── style.css
+
+
+add_action('wp_enqueue_scripts', function() {
+
+    wp_enqueue_style(
+        'theme-style',                               // unique handle
+        get_template_directory_uri() . '/assets/css/style.css', // file URL
+        [],                                          // dependencies (agar koi CSS pe depend nahi)
+        null                                         // version (null = default)
+    );
+
+});
