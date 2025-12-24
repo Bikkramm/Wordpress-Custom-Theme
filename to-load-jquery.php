c95acdae23129355a5e717dec1bebb2d2b654a50
Bikkramm <144205863+Bikkramm@users.noreply.github.com>
2025-12-24 10:36:31 +0530

    Update to-load-jquery.php

--- to-load-jquery.php
+++ to-load-jquery.php
@@ -1,8 +1,9 @@
-// note the added jquery should not be in $ format it should be with jQuery format
+/* note the added jquery should not be in $ format it should be with jQuery format
 for example:- my.js
 jQuery(document).ready(function() {
     alert('working');
 });
+*/
 
 <?php
 function myscript() {
